2b086f023f5c6e2cd006d80a5ba7e49d8bbdb47f
wncampos <97967036+undefined@users.noreply.github.com>
2022-02-15 00:12:29 +0000

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# exercicio3
-Created with WebComponents.dev
---
+++ src/app.js
@@ -0,0 +1,16 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+
+
+musicas = [
+      {cod:1, nome: "Talisma", artista:"Limao com Mel", album:"Acustico in Concert"},
+      {cod:2, nome: "Colo de Menina", artista:"Rastape", album:"Rastape"},
+      {cod:3, nome: "Ar Condicionado", artista:"Wesley Safadao", album:"WS In Miami Beach"},
+      {cod:4, nome: "No Toque do Celular", artista:"Avioes do Forro", album:"Avioes do Forro ao Vivo"},
+      {cod:5, nome: "Box com Varias Musicas", artista:"BOX Forró de Verdade", album:"60 anos de forró"}
+      
+        ];
+
+    
+}
